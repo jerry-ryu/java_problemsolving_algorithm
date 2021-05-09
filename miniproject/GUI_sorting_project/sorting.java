9ec3aa36c0d3f5f7bf9c9ed4683e43ccb6c0590f
freshmanbo <fbrjs15@naver.com>
2021-05-09 11:48:19 +0900

    Java GUI sorting program using java swing, awt and sorting algorithms. 90% complete -implement GUI Window and environmental preference(05/08) -implement Random function to initialize numbers(05/08) -implement Bubble sort function to sort numbers(05/08) -implement Selection sort, Insertion sort, Merge sort, Quick sort function to sort numbers(05/09) -implement swap function to guarantee reusability and measure constant time.
    left:
    - heap sort function
    -label for time and swap counter

--- miniproject/GUI_sorting_project/sorting.java
+++ miniproject/GUI_sorting_project/sorting.java
@@ -47,11 +47,10 @@ public class sorting extends JFrame {
 		t2 = new JMenuItem("랜덤"); // 숫자 다시 섞기
 		t2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 				Random();
 			}
 		});
-		
 
 		m2 = new JMenu("도움말");
 		t3 = new JMenuItem("실행 방법"); // 새로운 창으로 실행방법 소개
@@ -71,11 +70,12 @@ public class sorting extends JFrame {
 		setJMenuBar(jb);
 	}
 
+	// 콤보박스 버튼 설정
 	private void setButton() {
 		JPanel jp = new JPanel();
 		jp.setPreferredSize(new Dimension(500, 60));
 		jp.setBackground(Color.white);
-		String str[] = { "선택 정렬", "삽입 정렬", "버블 정렬", "합병 정렬 ", "퀵 정렬", "힙 정렬" };
+		String str[] = { "선택 정렬", "삽입 정렬", "버블 정렬", "합병 정렬", "퀵 정렬", "힙 정렬" };
 		JComboBox<String> com = new JComboBox<String>();
 		for (int i = 0; i < str.length; i++) {
 			com.addItem(str[i]);
@@ -88,9 +88,21 @@ public class sorting extends JFrame {
 
 				String menu = com.getSelectedItem().toString();
 				if (menu.equals("버블 정렬")) {
-
 					BubbleSort();
+				} else if (menu.equals("선택 정렬")) {
+
+					SelectionSort();
+				} else if (menu.equals("삽입 정렬")) {
+
+					InsertionSort();
+				} else if (menu.equals("합병 정렬")) {
+					int[] tmp = new int[panellist.length];
+
+					MergeSort(0, panellist.length - 1, tmp);
+				} else if(menu.equals("퀵 정렬")){
+					Quicksort();
 				}
+
 			}
 
 		});
@@ -101,6 +113,7 @@ public class sorting extends JFrame {
 		c.add(jp);
 	}
 
+	// 정렬될 패널 설정
 	private void setPanel() {
 		for (int i = 1; i < numlist.length + 1; i++) {
 			numlist[i - 1] = i;
@@ -115,32 +128,38 @@ public class sorting extends JFrame {
 
 	}
 
+	// 버블정렬
 	private void BubbleSort() {
 
 		try {
 			new Thread() {
 				public void run() {
-					for (int i = panellist.length-1; i > 0; i--) {
+					for (int i = panellist.length - 1; i > 0; i--) {
 						// 0 ~ (i-1)까지 반복
 						for (int j = 0; j < i; j++) {
 							panellist[j].setBackground(Color.red);
 							panellist[j + 1].setBackground(Color.red);
 							// j번째와 j+1번째의 요소가 크기 순이 아니면 교환
-							if (panellist[j].getHeight() < panellist[j + 1].getHeight()) {
+							if (panellist[j].getHeight() > panellist[j + 1].getHeight()) {
 								int tmp = panellist[j].getHeight();
 								panellist[j].setSize(1, panellist[j + 1].getHeight());
 								panellist[j + 1].setSize(1, tmp);
 							}
-							c.add(panellist[j]);
-							c.add(panellist[j + 1]);
-
 							repaint();
+
+							try {
+								Thread.sleep(1);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+
 							panellist[j].setBackground(Color.white);
 							panellist[j + 1].setBackground(Color.white);
 							repaint();
 						}
 					}
-					for(int i = 0; i<panellist.length; i++) {
+					for (int i = 0; i < panellist.length; i++) {
 						panellist[i].setBackground(Color.green);
 						c.add(panellist[i]);
 						try {
@@ -151,7 +170,7 @@ public class sorting extends JFrame {
 						}
 						repaint();
 					}
-					for(int i = 0; i<panellist.length; i++) {
+					for (int i = 0; i < panellist.length; i++) {
 						panellist[i].setBackground(Color.white);
 					}
 					repaint();
@@ -164,25 +183,309 @@ public class sorting extends JFrame {
 		}
 
 	}
+
+	// 선택정렬
+	private void SelectionSort() {
+
+		try {
+			new Thread() {
+				public void run() {
+					int indexMin;
+					for (int i = 0; i < panellist.length - 1; i++) {
+						indexMin = i;
+						panellist[i].setBackground(Color.red);
+						for (int j = i + 1; j < panellist.length; j++) {
+							panellist[j].setBackground(Color.red);
+							repaint();
+							if (panellist[j].getHeight() < panellist[indexMin].getHeight()) {
+								panellist[indexMin].setBackground(Color.white);
+								indexMin = j;
+								repaint();
+							} else {
+								panellist[j].setBackground(Color.white);
+								repaint();
+							}
+
+							try {
+								Thread.sleep(1);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+						int tmp = panellist[i].getHeight();
+						panellist[i].setSize(1, panellist[indexMin].getHeight());
+						panellist[indexMin].setSize(1, tmp);
+
+						panellist[i].setBackground(Color.white);
+						panellist[indexMin].setBackground(Color.white);
+						repaint();
+						try {
+							Thread.sleep(1);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					for (int i = 0; i < panellist.length; i++) {
+						panellist[i].setBackground(Color.green);
+						c.add(panellist[i]);
+						try {
+							Thread.sleep(3);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						repaint();
+					}
+					for (int i = 0; i < panellist.length; i++) {
+						panellist[i].setBackground(Color.white);
+					}
+					repaint();
+				}
+			}.start();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
+	// 삽입정렬
+	private void InsertionSort() {
+
+		try {
+			new Thread() {
+				public void run() {
+					for (int i = 1; i < panellist.length; i++) {
+						int j = i - 1;
+						int present = i;
+						while (j >= 0 && panellist[j].getHeight() > panellist[present].getHeight()) {
+							panellist[present].setBackground(Color.red);
+							panellist[j].setBackground(Color.red);
+							repaint();
+							try {
+								Thread.sleep(1);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							int tmp = panellist[present].getHeight();
+							panellist[present].setSize(1, panellist[j].getHeight());
+							panellist[j].setSize(1, tmp);
+							panellist[present].setBackground(Color.white);
+							panellist[j].setBackground(Color.white);
+							repaint();
+							present = j;
+							j--;
+						}
+						repaint();
+					}
+					for (int i = 0; i < panellist.length; i++) {
+						panellist[i].setBackground(Color.green);
+						c.add(panellist[i]);
+						try {
+							Thread.sleep(3);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						repaint();
+					}
+					for (int i = 0; i < panellist.length; i++) {
+						panellist[i].setBackground(Color.white);
+					}
+					repaint();
+				}
+			}.start();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
+	// 합병정렬-1 (쓰레드 생성 및 정렬 확인)
+	private void MergeSort(int start, int end, int[] tmp) {
+		try {
+			new Thread() {
+				public void run() {
+					Merge(start, end, tmp);
+					for (int i = 0; i < panellist.length; i++) {
+						panellist[i].setBackground(Color.green);
+						c.add(panellist[i]);
+						try {
+							Thread.sleep(3);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						repaint();
+					}
+					for (int i = 0; i < panellist.length; i++) {
+						panellist[i].setBackground(Color.white);
+					}
+					repaint();
+					
+				}
+
+			}.start();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+    
+	//합병정렬-2 (숫자 정렬)
+	private void Merge(int start, int end, int[] tmp) {
+		if (start < end) {
+			int mid = (start + end) / 2;
+			Merge(start, mid, tmp);
+			Merge(mid + 1, end, tmp);
+
+			int p = start;
+			int q = mid + 1;
+			int idx = p;
+
+			while (p <= mid || q <= end) {
+				if (q > end || (p <= mid && panellist[p].getHeight() < panellist[q].getHeight())) {
+					tmp[idx++] = panellist[p++].getHeight();
+				} else {
+					tmp[idx++] = panellist[q++].getHeight();
+				}
+			}
+			for (int i = start; i <= end; i++) {
+				panellist[i].setSize(1, tmp[i]);
+			}
+			for (int i = start; i <= end; i++) {
+				panellist[i].setBackground(Color.red);
+				repaint();
+				try {
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			for (int i = start; i <= end; i++) {
+				panellist[i].setBackground(Color.white);
+				repaint();
+				try {
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	//퀵정렬-1 (쓰레드 생성 및 정렬 확인)
+	private void Quicksort() {
+		try {
+			new Thread() {
+				public void run() {
+					quick(0, panellist.length-1);
+					for (int i = 0; i < panellist.length; i++) {
+						panellist[i].setBackground(Color.green);
+						try {
+							Thread.sleep(3);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						repaint();
+					}
+					for (int i = 0; i < panellist.length; i++) {
+						panellist[i].setBackground(Color.white);
+					}
+					repaint();
+					
+				}
+
+			}.start();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
 	
+	//퀵정렬-2 (숫자 정렬)
+	private void quick(int low, int high) {
+		if(low>=high) return;
+		
+		int mid = partition(low,high);
+		quick(low, mid-1);
+		quick(mid, high);
+	
+	}
+	
+	//퀵정렬-3 (pivot 구하기)
+	private int partition(int low, int high) {
+		
+		int pivot = panellist[(low+high)/2].getHeight();
+		panellist[(low+high)/2].setBackground(Color.blue);
+		while(low<=high) {
+			while(panellist[low].getHeight() < pivot) low++;
+			while(panellist[high].getHeight() > pivot) high--;
+			
+			if(low<=high) {
+				swap(low, high);
+				low++;
+				high--;
+			}
+		}
+		return low;
+	}
+
+	//힙정렬
+	
+	// 랜덤
 	private void Random() {
 		List<Integer> tmplist = new ArrayList<Integer>();
-		for(int i:numlist) {
+		for (int i : numlist) {
 			tmplist.add(i);
 		}
-		
+
 		Collections.shuffle(tmplist);
-		
-		for(int i=0; i<tmplist.size();i++) {
+
+		for (int i = 0; i < tmplist.size(); i++) {
 			numlist[i] = tmplist.get(i);
 		}
-		
-		for(int i=0; i<panellist.length; i++) {
+
+		for (int i = 0; i < panellist.length; i++) {
 			panellist[i].setSize(1, numlist[i]);
 		}
 	}
-	
 
+	//swap
+	private void swap(int a, int b) {
+		panellist[a].setBackground(Color.red);
+		panellist[b].setBackground(Color.red);
+		repaint();
+		int tmp = panellist[a].getHeight();
+		panellist[a].setSize(1, panellist[b].getHeight());
+		panellist[b].setSize(1, tmp);
+		
+		try {
+			Thread.sleep(3);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		panellist[a].setBackground(Color.white);
+		panellist[b].setBackground(Color.white);
+		
+	}
+	
+	// main
 	public static void main(String[] args) {
 		new sorting();
 	}
