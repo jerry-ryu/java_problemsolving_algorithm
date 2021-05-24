50508ef9ea86ef7147da49f0c389628c078cd5e3
freshmanbo <fbrjs15@naver.com>
2021-05-24 22:03:38 +0900

    solve Programmers_ConnectingIsland problem

---
+++ 문제풀이/Programmers_ConnectingIslands,21.05.24/Kruskal_solution.java
@@ -0,0 +1,74 @@
+package Programmers_ConnectingIslands;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Kruskal_solution {
+	
+	
+	//부모 노드를 가져옴
+	public int getParents(int set[], int x) {
+		if(set[x] == x) return x;
+		//재귀 함수를 통해 그래프 중에 가장 작은 부모 노드의 값을 가져온다
+		return set[x] = getParents(set, set[x]); 
+	}
+	
+	//부모 노드 병합
+	void unionParent(int set[], int a, int b) {
+		a = getParents(set, a);
+		b = getParents(set,b);
+		if(a<b) set[b] = a;
+		else set[a]=b;
+	}
+	
+	//같은 부모를 가지는지 확인
+	boolean find(int set[], int a, int b) {
+		a = getParents(set,a);
+		b = getParents(set,b);
+		if(a==b) return false;
+		else return true;
+	}
+	
+	public int solution(int n, int[][] costs) {
+		int answer = 0;
+		
+		 //연결되어 있는 섬 중 가장 작은 값을 나타내는 connect 배열
+        int[] connect = new int[n];
+        for(int i = 0; i<n; i++) {
+        	connect[i] = i;
+        }
+        
+        PriorityQueue<int[]> heap = new PriorityQueue<>(new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] a, int[] b) {
+				// TODO Auto-generated method stub
+				return a[2] - b[2];
+						
+			}
+        	
+        });
+        
+        for(int[] value : costs) {
+        	heap.add(value);
+        }
+        
+        //힙이 빌때까지(모든 간선 값을 볼때까지) 반복
+        while(!heap.isEmpty()) {
+        	int[] tmp = heap.poll();
+        	if(find(connect, connect[tmp[0]], connect[tmp[1]])) {
+        		unionParent(connect, connect[tmp[0]], connect[tmp[1]]);
+        		answer = answer + tmp[2];
+        	}
+        }
+		
+		return answer;
+	}
+	  public static void main(String[] args) {
+	    	
+	    	int[][] costs = {{0,2,1},{1,2,2},{2,3,2},{0,4,2},{2,4,2}} ;
+	    	MySolution sol = new MySolution();
+	    	System.out.println(sol.solution(5,costs));
+	    	
+	    }
+}
---
+++ 문제풀이/Programmers_ConnectingIslands,21.05.24/MySolution.java
@@ -0,0 +1,86 @@
+package Programmers_ConnectingIslands;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class MySolution {
+
+    public int solution(int n, int[][] costs) {
+        
+    	int answer = 0;
+    	
+        //연결되어 있는 섬 중 가장 작은 값을 나타내는 connect 배열
+    	//처음엔 연결된 다리가 없으므로, 연결되어 있는 섬 중 가장 작은 값은 자기 자신이다
+        int[] connect = new int[n];
+        for(int i = 0; i<n; i++) {
+        	connect[i] = i;
+        }
+        
+        //비용 값을 기준으로 최소 힙 구현 
+        PriorityQueue<int[]> heap = new PriorityQueue<>(new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] a, int[] b) {
+				// TODO Auto-generated method stub
+				return a[2] - b[2];
+						
+			}
+        	
+        });
+        
+        for(int[] value : costs) {
+        	heap.add(value);
+        }
+        
+        int allconnect = -1;
+        
+        //모든 섬이 연결될때까지 반복
+        while(allconnect!=0) {
+        	//비용이 가장 작은 순서대로 반환
+        	int[] tmp = heap.poll();
+        	
+        	//서로 연결되어있는 섬이 아니면 서로 연결
+        	if(connect[tmp[0]]!= connect[tmp[1]] ) {
+        		if(connect[tmp[0]]>connect[tmp[1]]) {
+        			update(connect, connect[tmp[1]],connect[tmp[0]]);
+        		}else {
+        			update(connect, connect[tmp[0]],connect[tmp[1]]);
+        		}
+        		
+        		
+        		
+        		answer = answer + tmp[2];
+        		
+        		//connect 배열의 모든 원소가 0(connect[0]의 값, 
+        		//노드중에 가장 작은 값)이면, 모든 노드가 연결된 것이다
+        		allconnect = connect[0];
+        		for(int con : connect) {
+        			if(con != allconnect) {
+        				allconnect = -1;
+        				break;
+        			}
+        		}
+        	}
+        }
+        
+        
+        return answer;
+    }
+    
+    //모든 이어진 섬들의 값을 가장 최소 노드 값으로 최신화
+    public void update(int[] connect, int newone, int oldone) {
+    	for(int i  = 0; i< connect.length; i++) {
+    		if(connect[i]==oldone) {
+    			connect[i] = newone;
+    		}
+    	}
+    }
+    
+    public static void main(String[] args) {
+    	
+    	int[][] costs = {{0,2,1},{1,2,2},{2,3,2},{0,4,2},{2,4,2}} ;
+    	MySolution sol = new MySolution();
+    	System.out.println(sol.solution(5,costs));
+    	
+    }
+}
